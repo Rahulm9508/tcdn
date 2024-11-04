5641d0fa31bd93109f2c68750afe8faecf6477d7
Rahulm9508 <153660317+Rahulm9508@users.noreply.github.com>
2024-11-04 11:12:49 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "akkiojhaprobot"
 BOT_TOKEN = "7667053478:AAGUm8yEaroZVT-utmZVeachNUv0uK1IMwM"
 API_ID = 20346550
 API_HASH = "bc79c3bea7a626887bdc0871eecf0327"
-MONGO_URI = "mongodb+srv://akbarakbar4329:sFqcKBFqzsYobYfu@cluster0.keqgcg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority"
